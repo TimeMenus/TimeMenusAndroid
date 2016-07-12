3c54510f3fca29f60beb278936b65ad7b3cb45b3
Abdin, Fadi <fabdin@express.com>
2016-07-12 10:02:57 -0400

    main view and a couple of buttons

--- app/src/main/java/com/express/apps/expresscafe/MainActivity.java
+++ app/src/main/java/com/express/apps/expresscafe/MainActivity.java
@@ -1,13 +1,40 @@
 package com.express.apps.expresscafe;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Button;
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.welness_web);
+
     }
+
+    public void openTodayMenu(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void openAdminLoginPage(View view){
+        Intent intent = new Intent(this,AdminLoginActivity.class);
+
+        startActivity(intent);
+    }
+
+
+
 }
