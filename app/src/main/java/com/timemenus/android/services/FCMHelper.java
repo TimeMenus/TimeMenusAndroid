89e557cf9a6cabdf769110832d3bc3c24851e839
kkatta <kkatta@express.com>
2016-11-30 13:45:20 -0500

    moved the notifications button to admin activity

--- app/src/main/java/com/timemenus/android/services/FCMHelper.java
+++ app/src/main/java/com/timemenus/android/services/FCMHelper.java
@@ -14,7 +14,9 @@ public class FCMHelper {
 
     private static final String URL_SEND = "https://fcm.googleapis.com/";
 
-    public static void sendNotification(JsonObject obj) {
+    public static String sendNotification(JsonObject obj) {
+
+        final String[] result = {new String()};
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(URL_SEND)
@@ -22,25 +24,23 @@ public class FCMHelper {
                 .build();
 
         APIService request = retrofit.create(APIService.class);
-
         Call<JsonObject> call = request.postCall(obj);
 
-        System.out.println("success message " + call.request().headers());
-
         call.enqueue(new Callback<JsonObject>() {
-
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                System.out.println("success");
+                result[0] = "success";
+                System.out.println("successfull");
             }
-
             @Override
             public void onFailure(Call<JsonObject> call, Throwable throwable) {
+                result[0] = "failure";
                 System.out.println("failure");
 
             }
         });
 
+        return result[0];
     }
 
 
